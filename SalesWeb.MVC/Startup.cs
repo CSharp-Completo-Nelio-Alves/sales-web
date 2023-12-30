59d4ac52b3c6305da51ac413c6307a403299be17
Tiago Pires <tiagopgu.pires@gmail.com>
2023-12-29 22:17:32 -0300

    Inserindo a classe Startup para configurações gerais da aplicação, já que a partir da versão 6 do .Net Core, esta classe foi abolida e toda configuração feita na classe Program. Porém, por questão de organização, a classe foi reinserida para concentrar todas as configurações.

---
+++ SalesWeb.MVC/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace SalesWeb.MVC
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{Id?}");
+            });
+        }
+    }
+}
